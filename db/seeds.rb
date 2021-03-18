64bae3ac0429014021051719f86a41b6cfc560f9
Jake <jake098877@gmail.com>
2021-03-18 16:48:08 -0400

    model, migration, seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name:"Sam John", username: "sjohn", email: "sjohn@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234")
-User.create(name:"Erving Smith", username: "esmith", email: "esmith@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234")
-User.create(name:"Hans Gettleman", username: "hgettlman", email: "hgettleman@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234")
+User.create(name:"Sam John", username: "sjohn", email: "sjohn@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234", password: "123456")
+User.create(name:"Erving Smith", username: "esmith", email: "esmith@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234", password: "123456")
+User.create(name:"Hans Gettleman", username: "hgettlman", email: "hgettleman@gmail.com", address: "123-abc st.", phone_numnber: "(123) 123-1234", password: "123456")
+
+Admin.create(username: "pianotech", email: "pianotech@tech.com", password: "123456")
