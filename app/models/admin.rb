f89c7e4f01bc48eebe6a558dd520ba952fffc09d
Jake <jake098877@gmail.com>
2021-03-19 20:44:19 -0400

    Added serializers for all the classes, changed admin table to devise.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,8 @@
 class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :users
   has_many :pianos, through: :users
   has_many :appointments
