4bb4d18ce4288f5daf60dcedfa180f8680209656
Jake <jake098877@gmail.com>
2021-03-21 22:44:24 -0400

    Revert "added all controllers, users index route"
    This reverts commit 06de226b7d5705979d0609977ce5bb9869c6de09.

--- Gemfile
+++ Gemfile
@@ -18,13 +18,13 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-gem 'devise'
+# gem 'devise'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'fast_jsonapi'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_004144) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -63,10 +60,6 @@ ActiveRecord::Schema.define(version: 2021_03_20_004144) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.text "name"
     t.text "username"
     t.text "address"
@@ -75,8 +68,5 @@ ActiveRecord::Schema.define(version: 2021_03_20_004144) do
     t.integer "number_of_pianos"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 
 end
