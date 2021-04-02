7398863bfc5529a24508d74f8257cad9e107fbcf
Jake <jake098877@gmail.com>
2021-04-01 22:43:02 -0400

    unknown additions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_184037) do
+ActiveRecord::Schema.define(version: 2021_03_30_021848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_184037) do
     t.text "private_technical_notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "make"
     t.index ["user_id"], name: "index_pianos_on_user_id"
   end
 
