951030943294be0dac0863fa92e97e9b8519aeb0
Jake <jake098877@gmail.com>
2021-04-04 23:20:09 -0400

    Refactors patch customer.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :notes, :category_id, :address, :password, :number_of_pianos, :id)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :notes, :category_id, :address, :password, :number_of_pianos, :id, :technician_notes)
   end
 
 end
